7ec30a60e3c903ab3581062b750e1df4777629b1
mishkamag <m93.maglaperidze@gmail.com>
2022-07-16 20:20:55 +0400

    add no expenses commit

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -16,6 +16,19 @@ const Expenses = (props) => {
     return expense.date.getFullYear().toString() === filteredYear;
   }); //პროდუქტის გაფილტვრა წლების მიხედვით
 
+  let expensesContet = <p>No expenses found.</p>;
+
+  if (filteredExpenses.length > 0) {
+    expensesContet = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ));
+  }
+
   return (
     <div>
       <Card className="expenses">
@@ -23,14 +36,7 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        {filteredExpenses.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
+        {expensesContet}
       </Card>
     </div>
   );
